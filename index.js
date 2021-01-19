ebe988a04a406427a427807808cf2d49cb31fff4
leoboyerbx <contact@leoboyer.fr>
2021-01-19 19:12:48 +0100

    Basic structure, end of first day

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-require('dotenv').config()
+const path = require('path')
+global.APP_ROOT = path.resolve(__dirname);
+
+// server http
+const server = require('./src/server')
+server.listen(3000, () => {
+    console.log("Server running on port 3000")
+})
+// connexion api twitter
+
+// traitements
